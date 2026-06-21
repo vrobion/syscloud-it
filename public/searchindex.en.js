1a51000528ae511ebdac43098ae491af4d65062b
vrobion <robionv@gmail.com>
2026-06-21 19:18:29 +0200

    Mise à jour du contenu

---
+++ public/searchindex.en.js
@@ -0,0 +1,42 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Accueil - SysCloud IT",
+    "content": "🖥️ Systèmes \u0026 Linux Bienvenue dans la section dédiée à l’administration système et à l’univers Linux. Vous trouverez ici mes guides de configuration, astuces pour Zorin OS / Ubuntu, et mes scripts utiles.\n📄 Pages disponibles : Le thème Relearn listera automatiquement ci-dessous toutes les fiches que nous allons ajouter dans ce dossier !",
+    "description": "🖥️ Systèmes \u0026 Linux Bienvenue dans la section dédiée à l’administration système et à l’univers Linux. Vous trouverez ici mes guides de configuration, astuces pour Zorin OS / Ubuntu, et mes scripts utiles.\n📄 Pages disponibles : Le thème Relearn listera automatiquement ci-dessous toutes les fiches que nous allons ajouter dans ce dossier !",
+    "tags": [],
+    "title": "Systèmes \u0026 Linux",
+    "uri": "/windows/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Bienvenue sur SysCloud IT 👋 Bienvenue sur mon espace de documentation personnel dédié aux systèmes, au réseau et au cloud. Ce site me sert de bloc-notes et de base de connaissances pour centraliser toutes mes configurations et astuces techniques.\n📂 Sommaire Général Le thème génère automatiquement le menu à gauche, mais voici les grandes sections à explorer :\n1. Microsoft 365 Accéder à la section Systèmes \u0026 Linux : Retrouvez mon guide de configuration Zorin OS et mes mémos de post-installation. ☁️ 2. Microsoft Azure À venir : Notes de déploiement, supervision et architecture cloud. ☁️ 3. Microsoft Intune À venir : Notes de déploiement, supervision et architecture cloud. 🌐 4. Sophos Firewall À venir : Protocoles, configurations de switchs/routeurs et bonnes pratiques de sécurité. 💡 Astuce : Utilise la barre de recherche en haut à gauche du menu pour retrouver instantanément un mémo ou une commande spécifique !",
+    "description": "Bienvenue sur SysCloud IT 👋 Bienvenue sur mon espace de documentation personnel dédié aux systèmes, au réseau et au cloud. Ce site me sert de bloc-notes et de base de connaissances pour centraliser toutes mes configurations et astuces techniques.\n📂 Sommaire Général Le thème génère automatiquement le menu à gauche, mais voici les grandes sections à explorer :",
+    "tags": [],
+    "title": "Accueil - SysCloud IT",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Accueil - SysCloud IT \u003e Systèmes \u0026 Linux",
+    "content": "Ma Post-Installation de Zorin OS 🚀 Voici les étapes et commandes que j’exécute juste après l’installation de mon système :\n1. Mise à jour complète sudo apt update \u0026\u0026 sudo apt upgrade -y",
+    "description": "Ma Post-Installation de Zorin OS 🚀 Voici les étapes et commandes que j’exécute juste après l’installation de mon système :\n1. Mise à jour complète sudo apt update \u0026\u0026 sudo apt upgrade -y",
+    "tags": [],
+    "title": "Configuration Zorin OS",
+    "uri": "/windows/configuration-zorin/index.html"
+  },
+  {
+    "breadcrumb": "Accueil - SysCloud IT",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Accueil - SysCloud IT",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
